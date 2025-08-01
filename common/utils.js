5f67057eba401e332bd71aa9ddf23d0563b34387
Rushabh Shah <shahrushabh1996@gmail.com>
2025-08-02 00:54:56 +0530

    "Fixed"

--- common/utils.js
+++ common/utils.js
@@ -14,7 +14,9 @@ const path = require('path');
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
+    region: process.env.AWS_REGION,
+    s3ForcePathStyle: false, // Use virtual-hosted-style URLs (regional)
+    signatureVersion: 'v4' // Use AWS Signature Version 4 (required for some regions)
 });
 
 // Initialize SNS client
